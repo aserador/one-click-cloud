c377f8db1c1567a4547ad798a7de9c7b82932573
Audric Serador <audricciel@gmail.com>
2024-02-23 20:54:43 -0600

    slightly fix descripitonbar

--- src/components/descriptionbar.tsx
+++ src/components/descriptionbar.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const DescriptionBar = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center">
       <div className="flex items-center justify-center w-full max-w-md">
         <input
-          className="w-full h-12 pl-4 pr-10 py-2 rounded-full text-lg focus:outline-none"
+          className="w-full h-6 pl-4 pr-10 rounded-xl text-lg focus:outline-none"
           type="text"
           placeholder="Describe your project here"
         />
