135473737a09b02447d50a15e183401436898964
Rohan <adada.rohan@gmail.com>
2024-02-24 13:16:36 -0600

    Finished MVP terraform

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -111,7 +111,57 @@ function s3hosting(bucket_name:string) {
 
 
   tfg.output('bucket_website_endpoint', {
+    description: 'The S3 bucket static hosting endpoint.',
     value: '${aws_s3_bucket_website_configuration.' + bucket_name + '.website_endpoint}'
+  });
+
+  return tfg
+}
+
+
+function cloudFrontHosting(bucket_name:string, cloudfront_name:string) {
+
+  const tfg = new TerraformGenerator({});
+
+  tfg.resource('aws_cloudfront_distribution', cloudfront_name, {
+    enabled: true,
+    is_ipv6_enabled: true,
+    origin: {
+      domain_name: '${aws_s3_bucket_website_configuration.' + bucket_name + '.website_endpoint}',
+      origin_id: '${aws_s3_bucket.' + bucket_name + '.bucket_regional_domain_name}',
+      custom_origin_config: {
+        http_port: 80,
+        https_port: 443,
+        origin_keepalive_timeout: 5,
+        origin_protocol_policy: "http-only",
+        origin_read_timeout: 30,
+        origin_ssl_protocols: [
+          "TLSv1.2",
+        ]
+      }
+    },
+    viewer_certificate: {
+      cloudfront_default_certificate: true
+    },
+    restrictions: {
+      geo_restriction: {
+        restriction_type: "none",
+        locations: []
+      }
+    },
+    default_cache_behavior: {
+      cache_policy_id: "658327ea-f89d-4fab-a63d-7e88639e58f6",
+      viewer_protocol_policy: "redirect-to-https",
+      compress: true,
+      allowed_methods: ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"],
+      cached_methods: ["GET", "HEAD"],
+      target_origin_id: '${aws_s3_bucket.' + bucket_name + '.bucket_regional_domain_name}'
+    }
+  });
+
+  tfg.output('cloudfront_domain_name', {
+    description: 'The domain name of the CloudFront distribution.',
+    value: '${aws_cloudfront_distribution.'+cloudfront_name+'.domain_name}'
   });
 
   return tfg
@@ -123,6 +173,7 @@ export default function handler(req: any, res: any) {
   const folder_name = 'tf_test_folder'
   const project_name = 'stratus'
   const bucket_name = "s3"
+  const cloudfront_name = "cloudfront"
 
   tfg.provider('aws', {
     region: 'us-east-2'
@@ -130,8 +181,9 @@ export default function handler(req: any, res: any) {
 
   var s3_tfg = s3generator(folder_name, project_name, bucket_name)
   var s3hosting_tfg = s3hosting(bucket_name)
+  var cloudfront_tfg = cloudFrontHosting(bucket_name, cloudfront_name)
 
-  tfg.merge(s3_tfg, s3hosting_tfg)
+  tfg.merge(s3_tfg, s3hosting_tfg, cloudfront_tfg)
 
   const result = tfg.generate();
   res.status(200).json({ terraformConfig: result.tf });
