d3becdea1e9457b25002cbb19654ac593fc3aa2e
abadia2 <abadia2@illinois.edu>
2024-02-24 16:38:44 -0600

    hacked session storage for download

--- src/pages/diagram.tsx
+++ src/pages/diagram.tsx
@@ -1,18 +1,27 @@
+import { useRouter } from "next/router";
 import Graph from "../components/Graph";
 import PersistentDrawerRight from "../components/PersistentDrawerRight";
 import StratusButton from "@/components/StratusButton";
 import { store } from "@/redux/store";
 
 const DiagramPage = () => {
+  const router = useRouter();
+
   return (
     <div>
       <PersistentDrawerRight />
       <Graph />
       <StratusButton
         onClick={() => {
-          store
-            .getState()
-            .persistentDrawerRight.AWSServices.filter((s) => !s?.disabled);
+          sessionStorage.setItem(
+            "graph",
+            JSON.stringify(
+              store
+                .getState()
+                .persistentDrawerRight.AWSServices.filter((s) => !s?.disabled)
+            )
+          );
+          router.push("/download");
         }}
       />
     </div>
--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -1,34 +1,55 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-function downloadTerraformFile(terraformCode:string) {
-    // Create a Blob object with Terraform code and the specific type
-    const blob = new Blob([terraformCode], { type: 'text/plain' });
-  
-    // Generate a temporary URL for the Blob object
-    const url = URL.createObjectURL(blob);
-  
-    return url;
-  }
+function downloadTerraformFile(terraformCode: string) {
+  // Create a Blob object with Terraform code and the specific type
+  const blob = new Blob([terraformCode], { type: "text/plain" });
+
+  // Generate a temporary URL for the Blob object
+  const url = URL.createObjectURL(blob);
+
+  return url;
+}
 
 export default function TerraformConfigPage() {
-  const [terraformURL, setTerraformURL] = useState('');
+  const [terraformURL, setTerraformURL] = useState("");
 
   useEffect(() => {
-    fetch('/api/terraform')
-      .then(response => response.json())
-      .then(data => setTerraformURL(downloadTerraformFile(data.terraformConfig)));
+    const hackedDiagramData = sessionStorage.getItem("graph");
+
+    if (!hackedDiagramData) {
+      alert("No diagram data found");
+    }
+
+    // TODO: Fix this
+    const jsonBody = JSON.parse(hackedDiagramData!);
+
+    console.log(jsonBody);
+
+    fetch("/api/terraform", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ services: jsonBody }),
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        setTerraformURL(downloadTerraformFile(data.terraformConfig))
+      );
   }, []);
 
   return (
-
     <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to <a className="text-blue-600" href="#">Stratus</a>
-        </h1>
+      <h1 className="text-6xl font-bold">
+        Welcome to{" "}
+        <a className="text-blue-600" href="#">
+          Stratus
+        </a>
+      </h1>
 
-        <a href={terraformURL} download="main.tf">Download Terraform file</a>
-
+      <a href={terraformURL} download="main.tf">
+        Download Terraform file
+      </a>
     </main>
-
   );
 }
