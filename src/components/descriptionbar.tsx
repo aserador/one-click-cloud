a4c9a69fd11efbfa5f7d7cc202a8414ecfa78e8f
Audric Serador <audricciel@gmail.com>
2024-02-23 21:33:08 -0600

    fix description bar

--- src/components/descriptionbar.tsx
+++ src/components/descriptionbar.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 const DescriptionBar = () => {
+  const handleSend = () => {
+    // Handle sending the data here
+  };
+
   return (
     <div className="flex items-center justify-center">
-      <div className="flex items-center justify-center w-full max-w-md">
-        <input
-          className="w-full h-6 pl-4 pr-10 rounded-xl text-lg focus:outline-none"
-          type="text"
-          placeholder="Describe your project here"
-        />
+      <div className="flex items-center w-full max-w-md">
+        <div className="relative flex w-full rounded-lg overflow-hidden">
+          <input
+            className="flex-grow pl-4 text-lg focus:outline-none"
+            type="text"
+            placeholder="Describe..."
+          />
+          <button
+            onClick={handleSend}
+            className="flex items-center justify-center w-12 text-lg"
+          >
+            <FontAwesomeIcon icon={faPaperPlane} />
+          </button>
+        </div>
       </div>
     </div>
   );
