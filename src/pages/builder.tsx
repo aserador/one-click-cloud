420a90fca0e52b5041b0cade22b22648c8eddfd5
Audric Serador <audricciel@gmail.com>
2024-02-24 14:01:56 -0600

    Fix text coloring

--- src/pages/builder.tsx
+++ src/pages/builder.tsx
@@ -5,7 +5,7 @@ const SearchPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <div className="flex flex-col items-center justify-center min-w-full">
-        <h1 className="text-6xl font-bold m-5 text-center">
+        <h1 className="text-6xl font-bold m-5 text-center text-white">
           Describe your project
         </h1>
         <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2 w-1/2 sm:w-2/3 md:w-1/2 lg:w-1/3">
