b3a1975cf1d5941d2a8c6d6356a7ac923ab58e27
Rohan <adada.rohan@gmail.com>
2024-02-24 12:59:34 -0600

    fully functioning static sites

--- playground/main.tf
+++ playground/main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "s3"{
 bucket_prefix = "stratus-"
 }
 
-resource "aws_s3_object" "object"{
+resource "aws_s3_object" "s3"{
 for_each = fileset("./tf_test_folder", "**")
 bucket = "${aws_s3_bucket.s3.bucket}"
 key = "${each.value}"
@@ -17,80 +17,62 @@ source = "./tf_test_folder/${each.value}"
 content_type = "${each.value}"
 }
 
-resource "aws_s3_bucket_cors_configuration" "s3" {
-  bucket = aws_s3_bucket.s3.id  
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET", "HEAD"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }  
+resource "aws_s3_bucket_cors_configuration" "s3"{
+bucket = "${aws_s3_bucket.s3.bucket}"
+cors_rule {
+allowed_headers = [
+"*"
+]
+allowed_methods = [
+"GET",
+"HEAD"
+]
+allowed_origins = [
+"*"
+]
+expose_headers = [
+"ETag"
+]
+max_age_seconds = 3000
+}
 }
 
-resource "aws_s3_bucket_acl" "s3" {
-    bucket = aws_s3_bucket.s3.id
-    acl    = "public-read"
-    depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+resource "aws_s3_bucket_acl" "s3"{
+bucket = "${aws_s3_bucket.s3.bucket}"
+acl = "public-read"
+depends_on = [
+aws_s3_bucket_ownership_controls.s3
+]
 }
 
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
-  bucket = aws_s3_bucket.s3.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-  depends_on = [aws_s3_bucket_public_access_block.example]
+resource "aws_s3_bucket_ownership_controls" "s3"{
+bucket = "${aws_s3_bucket.s3.bucket}"
+rule {
+object_ownership = "BucketOwnerPreferred"
+}
 }
 
-resource "aws_iam_user" "s3_bucket" {
-  name = "prod-media-bucket"
+resource "aws_iam_user" "s3"{
+name = "prod-s3-bucket"
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.s3.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+resource "aws_s3_bucket_public_access_block" "s3"{
+bucket = "${aws_s3_bucket.s3.bucket}"
+block_public_acls = false
+block_public_policy = false
+ignore_public_acls = false
+restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "s3_bucket" {
-    bucket = aws_s3_bucket.s3.id
-    policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Principal = "*"
-        Action = [
-          "s3:*",
-        ]
-        Effect = "Allow"
-        Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.s3.bucket}",
-          "arn:aws:s3:::${aws_s3_bucket.s3.bucket}/*"
-        ]
-      },
-      {
-        Sid = "PublicReadGetObject"
-        Principal = "*"
-        Action = [
-          "s3:GetObject",
-        ]
-        Effect   = "Allow"
-        Resource = [
-          "arn:aws:s3:::${aws_s3_bucket.s3.bucket}",
-          "arn:aws:s3:::${aws_s3_bucket.s3.bucket}/*"
-        ]
-      },
-    ]
-  })
-  
-  depends_on = [aws_s3_bucket_public_access_block.example]
+resource "aws_s3_bucket_policy" "s3"{
+bucket = "${aws_s3_bucket.s3.bucket}"
+policy = "{\"Version\":\"2012-10-17\",\"Statement\":[{\"Principal\":\"*\",\"Action\":[\"s3:*\"],\"Effect\":\"Allow\",\"Resource\":[\"arn:aws:s3:::${aws_s3_bucket.s3.bucket}\",\"arn:aws:s3:::${aws_s3_bucket.s3.bucket}/*\"]},{\"Sid\":\"PublicReadGetObject\",\"Principal\":\"*\",\"Action\":[\"s3:GetObject\"],\"Effect\":\"Allow\",\"Resource\":[\"arn:aws:s3:::${aws_s3_bucket.s3.bucket}\",\"arn:aws:s3:::${aws_s3_bucket.s3.bucket}/*\"]}]}"
+depends_on = [
+aws_s3_bucket_public_access_block.s3
+]
 }
 
-resource "aws_s3_bucket_website_configuration" "hosting"{
+resource "aws_s3_bucket_website_configuration" "s3"{
 bucket = "${aws_s3_bucket.s3.bucket}"
 index_document {
 suffix = "index.html"
@@ -98,5 +80,6 @@ suffix = "index.html"
 }
 
 output "bucket_website_endpoint"{
-value = "${aws_s3_bucket_website_configuration.hosting.website_endpoint}"
+value = "${aws_s3_bucket_website_configuration.s3.website_endpoint}"
 }
+
--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,65 +1,137 @@
-import { TerraformGenerator, fn} from 'terraform-generator';
+import { TerraformGenerator, fn, Argument} from 'terraform-generator';
 
-export default function handler(req: any, res: any) {
+function s3generator(folder_name:string, project_name:string, bucket_name:string) {
   const tfg = new TerraformGenerator({
   });
 
-  const folder_name = 'tf_test_folder'
 
-  tfg.provider('aws', {
-    region: 'us-east-2'
+  tfg.resource('aws_s3_bucket', bucket_name, {
+    bucket_prefix: project_name + '-',
   });
 
-  tfg.resource('aws_s3_bucket', 's3', {
-    bucket_prefix: 'my-tf-test-bucket'
-  });
-
-  tfg.resource('aws_s3_object', 'object', {
+  tfg.resource('aws_s3_object', bucket_name, {
     for_each : fn('fileset', './' + folder_name, '**'),
-    bucket: '${aws_s3_bucket.s3.bucket}',
+    bucket: '${aws_s3_bucket.' + bucket_name + '.bucket}',
     key: "${each.value}",
-    source : './' + folder_name + '/${each.value}'
+    source : './' + folder_name + '/${each.value}',
+    content_type: "${each.value}"
     })
 
+  return tfg
+}
+
+function s3hosting(bucket_name:string) {
+
+  const tfg = new TerraformGenerator({});
   
-  tfg.resource('aws_s3_bucket_website_configuration', 'hosting', {
-    bucket: '${aws_s3_bucket.s3.bucket}',
-    index_document: {
-      suffix: 'index.html'
+  tfg.resource('aws_s3_bucket_cors_configuration', bucket_name, {
+    bucket: '${aws_s3_bucket.'+ bucket_name + '.bucket}',
+    cors_rule: [
+      {
+        allowed_headers: ["*"],
+        allowed_methods: ["GET", "HEAD"],
+        allowed_origins: ["*"],
+        expose_headers: ["ETag"],
+        max_age_seconds: 3000
+      }
+    ]
+  });
+
+  const acl_dependencies = [new Argument("aws_s3_bucket_ownership_controls." + bucket_name )];
+
+  tfg.resource('aws_s3_bucket_acl', bucket_name, {
+    bucket: '${aws_s3_bucket.'+ bucket_name + '.bucket}',
+    acl: "public-read",
+    depends_on: acl_dependencies
+  });
+
+  
+  tfg.resource("aws_s3_bucket_ownership_controls", bucket_name, {
+    bucket: '${aws_s3_bucket.'+ bucket_name + '.bucket}',
+    rule: {
+      object_ownership: "BucketOwnerPreferred"
     }
   });
 
-  tfg.resource('aws_s3_bucket_public_access_block', 'public_access_block', {
-    bucket: '${aws_s3_bucket.s3.bucket}',
+  tfg.resource("aws_iam_user", bucket_name, {
+    name: "prod-" + bucket_name + "-bucket"
+  });
+
+  tfg.resource("aws_s3_bucket_public_access_block",  bucket_name, {
+    bucket: '${aws_s3_bucket.'+ bucket_name + '.bucket}',
     block_public_acls: false,
     block_public_policy: false,
     ignore_public_acls: false,
     restrict_public_buckets: false
   });
 
-  tfg.resource('aws_s3_bucket_policy', 'bucket_policy', {
-    bucket: '${aws_s3_bucket.s3.bucket}',
+  const policy_dependencies = [new Argument("aws_s3_bucket_public_access_block." + bucket_name )];
+
+  tfg.resource("aws_s3_bucket_policy", bucket_name, {
+    bucket: '${aws_s3_bucket.'+ bucket_name + '.bucket}',
     policy: JSON.stringify({
-      "Version" : "2012-10-17",
-      "Statement" : [
+      Version: "2012-10-17",
+      Statement: [
         {
-          "Sid" : "PublicReadGetObject",
-          "Effect" : "Allow",
-          "Principal" : "*",
-          "Action" : "s3:GetObject",
-          "Resource" : "arn:aws:s3:::${aws_s3_bucket.s3.bucket}/*"
-        }
+          Principal: "*",
+          Action: [
+            "s3:*",
+          ],
+          Effect: "Allow",
+          Resource: [
+            "arn:aws:s3:::${aws_s3_bucket."+ bucket_name + ".bucket}",
+            "arn:aws:s3:::${aws_s3_bucket."+ bucket_name + ".bucket}/*"
+          ]
+        },
+        {
+          Sid: "PublicReadGetObject",
+          Principal: "*",
+          Action: [
+            "s3:GetObject",
+          ],
+          Effect: "Allow",
+          Resource: [
+            "arn:aws:s3:::${aws_s3_bucket." + bucket_name + ".bucket}",
+            "arn:aws:s3:::${aws_s3_bucket." + bucket_name + ".bucket}/*"
+          ]
+        },
       ]
-    })
+    }),
+
+    depends_on: policy_dependencies
   });
 
-  tfg.output('bucket_name', {
-    value: '${aws_s3_bucket.s3.bucket}'
+
+  tfg.resource('aws_s3_bucket_website_configuration', bucket_name, {
+    bucket: '${aws_s3_bucket.'+ bucket_name + '.bucket}',
+    index_document: {
+      suffix: 'index.html'
+    }
   });
+
 
   tfg.output('bucket_website_endpoint', {
-    value: '${aws_s3_bucket_website_configuration.hosting.website_endpoint}'
+    value: '${aws_s3_bucket_website_configuration.' + bucket_name + '.website_endpoint}'
   });
+
+  return tfg
+}
+
+export default function handler(req: any, res: any) {
+  const tfg = new TerraformGenerator({});
+
+  const folder_name = 'tf_test_folder'
+  const project_name = 'stratus'
+  const bucket_name = "s3"
+
+  tfg.provider('aws', {
+    region: 'us-east-2'
+  });
+
+  var s3_tfg = s3generator(folder_name, project_name, bucket_name)
+  var s3hosting_tfg = s3hosting(bucket_name)
+
+  tfg.merge(s3_tfg, s3hosting_tfg)
 
   const result = tfg.generate();
   res.status(200).json({ terraformConfig: result.tf });
