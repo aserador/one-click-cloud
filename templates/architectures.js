cb87d6eae3f4ea9ec1495ef73607a8d72bf9550e
Audric Serador <audricciel@gmail.com>
2024-02-25 01:55:09 -0600

    Update architectures.js

--- templates/architectures.js
+++ templates/architectures.js
@@ -34,28 +34,6 @@ export const ARCHITECTURES = [
             }
         ]
     },
-        {
-        "name": "S3 + CloudFront",
-        "description": "This bundle combines Amazon S3 and Amazon CloudFront. S3 provides secure, scalable object storage, while CloudFront is a fast content delivery network (CDN) service that securely delivers data, videos, applications, and APIs to users globally with low latency and high transfer speeds. This combination is ideal for applications that require both object storage and fast content delivery to users around the world.",
-        "pros": [
-            "Highly scalable",
-            "Secure",
-            "Durable",
-            "Cost-effective",
-            "Fast content delivery"
-        ],
-        "cons": [
-            "Not suitable for transactional data",
-            "Limited support for complex queries"
-        ],
-        "services": [0, 1],
-        "edges": [
-            {
-                "from": 0,
-                "to": 1
-            }
-        ]
-    },
     {
         "name": "Google Cloud Storage",
         "description": "Store your static files on Cloud Storage and make them publicly accessible.",
