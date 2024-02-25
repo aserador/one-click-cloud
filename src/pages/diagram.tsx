eb9f548b20642b761eb74a01993645476cb2d9a4
abadia2 <abadia2@illinois.edu>
2024-02-25 03:44:15 -0600

    graph validation

--- src/pages/diagram.tsx
+++ src/pages/diagram.tsx
@@ -19,6 +19,111 @@ import {
 } from "@/redux/persistentDrawerRightSlice";
 import StratusTextField from "@/components/StratusTextField";
 import _ from "lodash";
+import { IGraphEdge } from "@/redux/models";
+import { toast } from "react-toastify";
+
+const getNode = (id: string, services: any[]) => {
+  for (const service of services) {
+    if (String(service.id) === String(id)) {
+      return service;
+    }
+  }
+  return null;
+};
+
+const validateEdges = (nodes: any[], edges: IGraphEdge[]) => {
+  // Check for duplicate source 
+  const sourceSet = new Set();
+  for (const e1 of edges) {
+    if (sourceSet.has(e1.source)) {
+      toast.error(`${getNode(e1.source, nodes)?.name} cannot target more than one service.`);
+      return false;
+    }
+    sourceSet.add(e1.source);
+  }
+
+  // Check for duplicate target
+  const targetSet = new Set();
+  for (const e2 of edges) {
+    if (targetSet.has(e2.target)) {
+      toast.error(`${getNode(e2.target, nodes)?.name} cannot be targeted by more than one service.`);
+      return false;
+    }
+    targetSet.add(e2.target);
+  }
+
+  // Check for invalid connections
+  for (const edge of edges) {
+    const source = getNode(edge.source, nodes);
+    const target = getNode(edge.target, nodes);
+
+    if (edge.source === edge.target) {
+      toast.error(`${source?.name} cannot target itself.`);
+      return false;
+    }
+
+    if (source.name == "S3") {
+      if (target.name == "Route 53") {
+        toast.error(`S3 cannot directly target Route 53.`);
+        return false;
+      } else if (target.name == "S3") {
+        toast.error(`S3 cannot directly target S3.`);
+        return false;
+      }
+    } else if (source.name == "CloudFront") {
+      if (target.name == "S3") {
+        toast.error(`CloudFront cannot directly target S3.`);
+        return false;
+      } else if (target.name == "CloudFront") {
+        toast.error(`CloudFront cannot directly target CloudFront.`);
+        return false;
+      }
+    } else if (source.name == "Route 53") { 
+      if (target.name == "S3") {
+        toast.error(`Route 53 cannot directly target S3.`);
+        return false;
+      } else if (target.name == "CloudFront") {
+        toast.error(`Route 53 cannot directly target CloudFront.`);
+        return false;
+      } else if (target.name == "Route 53") {
+        toast.error(`Route 53 cannot directly target Route 53.`);
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+const validateNodes = (services: any[]) => {
+  // Check for duplicate names for S3, CloudFront, and Route 53
+  const s3Set = new Set();
+  const cloudFrontSet = new Set();
+  const route53Set = new Set();
+  for (const service of services) {
+    if (String(service.id) === String(0)) {
+      // TODO: Fix order dependency or array of questions
+      if (s3Set.has(service.questions[0].value)) {
+        toast.error("One or more S3 services have the same Nickname. Please rename the services to be unique.");
+        return false;
+      }
+      s3Set.add(service.questions[0].value);
+    } else if (String(service.id) === String(1)) {
+      if (cloudFrontSet.has(service.questions[0].value)) {
+        toast.error("CloudFront cannot be added more than once.");
+        return false;
+      }
+      cloudFrontSet.add(service.questions[0].value);
+    } 
+    else if (String(service.id) === String(2)) {
+      if (route53Set.has(service.settings.domain.value)) {
+        toast.error("Route 53 cannot be added more than once.");
+        return false;
+      }
+      route53Set.add(service.settings.domain.value);
+    }
+  }
+  return true;
+}
 
 const DiagramPage = () => {
   // Local state for diagram (nodes are aws services, edges are dependencies)
@@ -177,6 +282,25 @@ const DiagramPage = () => {
       <StratusButton
         classStyles="absolute bottom-8 left-8"
         onClick={() => {
+
+          // Check if at least one service is added to the diagram
+          if (graphServices.length === 0) {
+            toast.error("Please add at least one service to the diagram.");
+            return;
+          }
+
+          if (graphServices.find((s) => s.name === "S3") === undefined) {
+            toast.error("S3 is required in the diagram. Please add S3 to the diagram.");
+            return;
+          }
+
+          if (!validateEdges(graphServices, graphEdges)) {
+            return;
+          }
+
+          if (!validateNodes(graphServices)) {
+            return;
+          }
           sessionStorage.setItem(
             "graphServices",
             JSON.stringify(graphServices.filter((s) => !s?.disabled))
