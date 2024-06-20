5496d361b67cf714bdea2187192513add5f33731
abadia2 <abadia2@illinois.edu>
2024-06-20 11:13:15 -0500

    category field added to schema

--- src/components/Designer/Diagram/index.tsx
+++ src/components/Designer/Diagram/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import Graph from './Graph';
+import { useAppStore } from '@/redux/hooks';
+import { IGraphNode, IGraphNodeData, NodeType } from '@/redux/designer/models';
 
 function Diagram() {
+  const store = useAppStore();
+  const initialGraphNodes = store.getState().graph.graphNodes;
+  
+  
+  const initialServices: Array<IGraphNode> = Object.keys(initialGraphNodes).map(
+    (nodeId: string, index: number) => {
+      return {
+        id: nodeId,
+        type: NodeType.ICON,
+        position: { 
+          x: 120 * (index + 1), 
+          y: 80 * (index + 1)
+        },
+        data: { 
+          label: initialGraphNodes[nodeId].id, 
+          category: initialGraphNodes[nodeId].category,
+          service: initialGraphNodes[nodeId].id,
+        } as IGraphNodeData,
+      }
+    }
+  );
+  const initialEdges = store.getState().graph.graphEdges;
+  
   return (
     <div className="flex-1 bg-black">
       <Graph
