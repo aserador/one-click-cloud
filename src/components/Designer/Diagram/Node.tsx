aebbcaa02be178c7e24db8cf62a4d0930a6f25a9
abadia2 <abadia2@illinois.edu>
2024-06-21 06:56:12 -0500

    handles only appear on hover

--- src/components/Designer/Diagram/Node.tsx
+++ src/components/Designer/Diagram/Node.tsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
-import { Handle, NodeProps, Position } from 'reactflow';
+import { Handle, HandleType, NodeProps, Position } from 'reactflow';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
-import { getFocusedNodeId, setFocusedNodeId } from '@/redux/designer/slice/graphSlice';
+import {
+  getFocusedNodeId,
+  setFocusedNodeId,
+} from '@/redux/designer/slice/graphSlice';
 import { IGraphNodeData } from '@/redux/designer/models';
 
+interface IFancyHandleProps {
+  id: string;
+  position: Position;
+  handleType: HandleType;
+  isHidden: boolean;
+}
+
+function FancyHandle({
+  id,
+  position,
+  handleType,
+  isHidden,
+}: IFancyHandleProps) {
+  const hiddenStyle = isHidden ? 'opacity-0' : '';
+  return (
+    <Handle
+      className={hiddenStyle}
+      type={handleType}
+      position={position}
+      id={id}
+    />
+  );
+}
+
 export function IconNode({ id, data }: NodeProps<IGraphNodeData>) {
+  const [hideHandles, setHideHandles] = useState(true);
+
   const dispatch = useAppDispatch();
   const focusedNodeId = useAppSelector(getFocusedNodeId);
   const onClick = () => focusedNodeId !== id && dispatch(setFocusedNodeId(id));
 
   return (
-    <div onClick={onClick}>
-      <Handle 
-        type="target" 
-        position={Position.Top}
-      />
+    <div
+      onClick={onClick}
+      onMouseEnter={() => setHideHandles(false)}
+      onMouseLeave={() => setHideHandles(true)}
+    >
       <div>
         <Image
           src={`svg/aws/${data.category}/${data.service}.svg`}
@@ -24,10 +53,29 @@ export function IconNode({ id, data }: NodeProps<IGraphNodeData>) {
           height={24}
         />
       </div>
-      <Handle 
-        type="source" 
-        position={Position.Bottom} 
-        id="a" 
+      <FancyHandle
+        handleType="target"
+        position={Position.Left}
+        id="left"
+        isHidden={hideHandles}
+      />
+      <FancyHandle
+        handleType="target"
+        position={Position.Bottom}
+        id="bottom"
+        isHidden={hideHandles}
+      />
+      <FancyHandle
+        handleType="source"
+        position={Position.Right}
+        id="right"
+        isHidden={hideHandles}
+      />
+      <FancyHandle
+        handleType="source"
+        position={Position.Top}
+        id="top"
+        isHidden={hideHandles}
       />
     </div>
   );
