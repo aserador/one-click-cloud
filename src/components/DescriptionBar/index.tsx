126df672876067b533120da928e0a46cb15a42c6
abadia2 <abadia2@illinois.edu>
2024-06-22 07:45:45 -0500

    change form submission logic to allow for 'enter' to submit

--- src/components/DescriptionBar/index.tsx
+++ src/components/DescriptionBar/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 type DescriptionBarProps = {
   input: string;
@@ -16,27 +16,27 @@ const DescriptionBar: React.FC<DescriptionBarProps> = ({
   const maxLength = 100;
 
   return (
-<div className="flex items-center justify-center">
-  <div className="flex flex-col items-start w-full max-w-md">
-    <div className="relative flex w-full">
-      <input
-        className="flex-grow px-4 py-2 text-white bg-transparent border-b border-white focus:outline-none"
-        type="text"
-        placeholder="eg. React.js with Tailwind"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        maxLength={maxLength}
-      />
-      <button
-        onClick={handleSend}
-        className="flex items-center justify-center w-12 text-lg text-white bg-transparent border-b border-white"
-      >
-        <FontAwesomeIcon icon={faChevronRight} />
-      </button>
+    <div className="flex items-center justify-center">
+      <div className="flex flex-col items-start w-full max-w-md">
+        <form className="relative flex w-full" onSubmit={handleSend}>
+          <input
+            className="flex-grow px-4 py-2 text-white bg-transparent border-b border-white focus:outline-none"
+            type="text"
+            placeholder="eg. React.js with Tailwind"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            maxLength={maxLength}
+          />
+          <button
+            type="submit"
+            className="flex items-center justify-center w-12 text-lg text-white bg-transparent border-b border-white"
+          >
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
+        </form>
+        <p className="text-white">{`${input.length}/${maxLength}`}</p>
+      </div>
     </div>
-    <p className="text-white">{`${input.length}/${maxLength}`}</p>
-  </div>
-</div>
   );
 };
 
